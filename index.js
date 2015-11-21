22ac0b2469b8659f7d9b3932df87392d2ac58458
Josh Castle <joshrcastle@gmail.com>
2015-11-20 19:16:35 -0600

    First commit.

---
+++ index.js
@@ -0,0 +1,32 @@
+Array.prototype.add = function(obj){
+	this.push(obj);
+}
+
+Array.prototype.remove = function(obj){
+	for (var i=0; i<this.length; i++){
+		if (this[i] === obj){
+			this.splice(i, 1);
+			return;
+		}
+	}
+}
+
+Array.prototype.removeAll = function(obj){
+	while (this.contains(obj)) this.remove(obj);
+}
+
+Array.prototype.get = function(index){
+	return this[index];
+}
+
+Array.prototype.size = function(){
+	return this.length;
+}
+
+Array.prototype.contains = function(obj){
+	for (var i=0; i<this.length; i++){
+		if (this[i] === obj) return true;
+	}
+
+	return false;
+}
